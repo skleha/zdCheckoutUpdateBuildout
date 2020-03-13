2507cf251064b71c0aa3aa1472174ff27b6d1678
thejenkdog <thejenkdog@gmail.com>
2020-03-13 14:30:54 -0700

    Buildout of payment info continued

--- frontend/components/PaymentUpdate.jsx
+++ frontend/components/PaymentUpdate.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 function PaymentUpdate(props) {
+
+  const [{number, exp, cvv}, setPaymentInfo] = useState({number: 1234, exp: "12/23", cvv: "813"});
+
+
+  
 
 
 
@@ -12,9 +17,12 @@ function PaymentUpdate(props) {
       <div className="payments-title">Update Payment Information</div>
       
       <div className="payment-inputs-div">
-        <input className="payment-input" id="credit-card-no" type="text" />
-        <input className="payment-input" id="credit-card-exp" type="text" />
-        <input className="payment-input" id="credit-card-cvv" type="text" />
+        <label className="payment-label" htmlFor="credit-card-no">Credit Card Number:</label>
+        <input className="payment-input" id="credit-card-no" type="text" value={number}/>
+        <label className="payment-label" htmlFor="credit-card-exp">Expiration (mm/yy)</label>
+        <input className="payment-input" id="credit-card-exp" type="text" value={exp}/>
+        <label className="payment-label" htmlFor="credit-card-cvv">CVV</label>
+        <input className="payment-input" id="credit-card-cvv" type="text" value={cvv}/>
       </div>
 
     </div>
