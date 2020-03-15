d023bb0d953dd1bc47734b297cbe5227bb1380fa
thejenkdog <thejenkdog@gmail.com>
2020-03-14 17:45:28 -0700

    Fix bug in validateSeats helperfunc

--- frontend/helpers/helperFuncs.js
+++ frontend/helpers/helperFuncs.js
@@ -1,12 +1,12 @@
 
-export const hasSubscriptionChanged = (selectedPlan, currentPlan) => {
-  const hasPlanChanged = selectedPlan.plan !== currentPlan.plan;
-  const hasSeatsChanged = Number(selectedPlan.seats) !== Number(currentPlan.seats);
-  const hasCostChanged = selectedPlan.cost !== currentPlan.cost;
+export const hasSubscriptionChanged = (selectedSub, currentSub) => {
+  const hasPlanChanged = selectedSub.plan !== currentSub.plan;
+  const hasSeatsChanged = Number(selectedSub.seats) !== Number(currentSub.seats);
+  const hasCostChanged = selectedSub.cost !== currentSub.cost;
   return { hasPlanChanged, hasSeatsChanged, hasCostChanged }
 };
 
 export const validateSeatNum = seats => {
   const typeConvertedSeats = Number(seats);
-  return Number(typeConvertedNum).isInteger && typeConvertedSeats > 0;
+  return Number.isInteger(typeConvertedSeats) && typeConvertedSeats > 0;
 }
